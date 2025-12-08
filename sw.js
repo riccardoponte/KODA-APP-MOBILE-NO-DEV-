3e8a0467d717f7af7ce7a2f2a2cf3454ad4c72f0
riccardoponte <riccardoponte@users.noreply.github.com>
2025-12-08 11:28:04 +0000

    deploy: eb0b949e7322c25ba94fabcfa1c106b2be551745

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'koda-ai-cache-v1';
 const urlsToCache = [
     '/KODA-APP-MOBILE-NO-DEV-/',
     '/KODA-APP-MOBILE-NO-DEV-/index.html',
-    // Aggiungi qui file statici se ne hai
+    // Aggiungi qui files statici se ne hai
     // Esempio: '/style.css', '/app.js', '/fonts/Inter-Regular.ttf'
     // Per ora, lasciamo solo i file principali.
 ];
